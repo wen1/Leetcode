c37ba6e2f0969453e9a5f1166e7e3d440f11f704
wen1 <jdwen1@gmail.com>
2013-06-14 20:58:03 -0300

    Update ZigZag Conversion.cpp

--- ZigZag Conversion.cpp
+++ ZigZag Conversion.cpp
@@ -1,3 +1,7 @@
+// Type your C++ code and click the "Run Code" button!
+// Your code output will be shown on the left.
+// Click on the "Show input" button to enter input data to be read (from stdin).
+
 #include <string>
 #include <vector>
 using namespace std;
@@ -21,7 +25,20 @@ public:
 ////////////////////////////////////////////////////////////////////////
         for(int i = 1; i<nRows; i++) rowNum[i] += rowNum[i-1];
         for(int i =0; i<rowNum.size(); i++) cout << rowNum[i] << " ";
-        
+        cout << endl;
+////////////////////////////////////////////////////////////////////////
+        for(int i = 0; i< s.size(); i++){
+            int tempRowID = i % magic;
+            tempRowID = (tempRowID < nRows? tempRowID : magic-tempRowID);
+            int tempColID = i / magic;
+            int temp = i % magic;
+            tempColID = (temp < nRows? ((temp == 0 || temp == nRows-1)? tempColID : tempColID * 2) : tempColID*2 + 1);
+            int newID = (tempRowID == 0? 0 : rowNum[tempRowID-1]) + tempColID;
+            ss[newID] = s[i];
+            //
+            cout << "new:\t" << newID << "\told:\t" << i << endl;
+        }
+        cout << ss << endl;
         return "ss";
     }
 };
