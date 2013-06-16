ee6e611589c8f75f2c2054bf81f7f9f6c215040f
wen1 <jdwen1@gmail.com>
2013-06-15 21:28:01 -0300

    Update Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -1,7 +1,33 @@
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <math>
+using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
-            
+        if (x == 0) return 0;
+        int sign = x / abs(x);
+        string s = integerToString(abs(x));
+        std::reverse(s.begin(),s.end());
+        int res = stringToInt(s);
+        return sign * res;
     }
-}
+    // return a string of the given int
+    // ex: integerToString(10) -> "10"
+    string integerToString(int a){
+        stringstream res;
+        res << a;
+        return res.str();
+    }
+    // return a int of the given string
+    // ex: stringToInt("01") -> 1
+    int stringToInt(string s){
+        stringstream temp;
+        temp << s;
+        int res;
+        temp >> res;
+        return res;
+    }
+};
